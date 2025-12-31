6d3d2b8a9545a87c29739f35aee23768423477ad
23f3002360 <23f3002360@ds.study.iitm.ac.in>
2026-01-01 00:26:32 +0530

    remove vercel build output

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.D22rVtKH.js","_app/immutable/chunks/BTUvMHCI.js","_app/immutable/chunks/DiJjCzxR.js","_app/immutable/chunks/DPOIDbx0.js","_app/immutable/chunks/BpDq2_1f.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.BFFWikA5.js","_app/immutable/chunks/BTUvMHCI.js","_app/immutable/chunks/DiJjCzxR.js","_app/immutable/chunks/_RNEUA52.js","_app/immutable/chunks/Bww4oMDO.js","_app/immutable/chunks/BwfKOnHn.js","_app/immutable/chunks/XjBNncT7.js","_app/immutable/chunks/DhmSQ-8C.js"];
-export const stylesheets = [];
-export const fonts = [];
